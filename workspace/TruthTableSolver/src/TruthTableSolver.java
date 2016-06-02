99bd2d5dce3b58f7a867ab715aa537b33c037737
William Xiong <William Xiong>
2016-06-01 19:09:39 -0500

    testong

---
+++ workspace/TruthTableSolver/src/TruthTableSolver.java
@@ -0,0 +1,18 @@
+import java.util.*;
+public class TruthTableSolver {
+public static boolean solveTable(boolean a, boolean b, boolean c, boolean d){
+
+	boolean function = !((a && !(!b && c)) || (!(!b && c) || !d));
+	
+	
+	
+	
+	
+	return function;
+}
+	public static void main(String[] args){
+		
+		
+	}
+	
+}
